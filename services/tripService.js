9cd4205ebc2758b09d26e8d1030ae8f7b8753a71
Srirupa <srirupadatta@pursuit.org>
2019-05-15 21:49:55 -0400

    added function to create a trip

--- services/tripService.js
+++ services/tripService.js
@@ -1,6 +1,14 @@
 const db = require('./dbConnect');
 const TripService = {};
 
+TripService.create = (name, country, city, departure_date, return_date, user_id) => {
+    const sql = `
+    INSERT INTO trip (name, country, city, departure_date, return_date, user_id)
+    VALUES ($[name], $[country], $[city], $[departure_date], $[return_date], $[user_id])
+    RETURNING id;
+    `;
+    return db.one(sql, { name, country, city, departure_date, return_date, user_id })
+}
 
 
 module.exports = TripService;
