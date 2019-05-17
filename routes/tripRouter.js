5298e89587541a9714d28e081b757821f2673d37
Srirupa <srirupadatta@pursuit.org>
2019-05-16 23:49:47 -0400

    added route to create a trip

--- routes/tripRouter.js
+++ routes/tripRouter.js
@@ -3,5 +3,18 @@ const tripRouter = express.Router();
 const TripService = require('../services/tripService');
 
 
+tripRouter.post('/', (req, res, next) => {
+    const { name, country, city, departure_date, return_date, user_id } = req.body;
+
+    TripService.create(name, country, city, departure_date, return_date, user_id)
+        .then(({ id }) => {
+            res.status(200);
+            res.json({ id });
+        })
+        .catch(err => {
+            next(err);
+        })
+});
+
 
 module.exports = tripRouter;
