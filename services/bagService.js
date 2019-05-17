31ae37af21b60c4d3666111612487819b6c4d973
Srirupa <srirupadatta@pursuit.org>
2019-05-17 01:20:43 -0400

    added function to create a bag

--- services/bagService.js
+++ services/bagService.js
@@ -1,6 +1,13 @@
 const db = require('./dbConnect');
 const BagService = {};
 
-
+BagService.create = (trip_id, type_id) => {
+    const sql = `
+    INSERT INTO bag (trip_id, type_id)
+    VALUES ($[trip_id], $[type_id])
+    RETURNING id;
+    `;
+    return db.one(sql, { trip_id, type_id });
+}
 
 module.exports = BagService;
