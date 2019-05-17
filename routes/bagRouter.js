913de31fa8bef261c955375c7e2068bd7cab5e7b
Srirupa <srirupadatta@pursuit.org>
2019-05-17 01:21:24 -0400

    added route to create a bag

--- routes/bagRouter.js
+++ routes/bagRouter.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bagRouter = express.Router();
 const BagService = require('../services/bagService');
 
+bagRouter.post('/', (req, res, next) => {
+    const { trip_id, type_id } = req.body;
 
+    BagService.create(trip_id, type_id)
+        .then(({ id }) => {
+            res.status(200);
+            res.json({ id });
+        })
+        .catch(err => {
+            next(err);
+        })
+});
 
 module.exports = bagRouter;
