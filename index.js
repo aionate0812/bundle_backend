1fc04b2c00d89632a06cd010ccf644427c3881dd
Srirupa <srirupadatta@pursuit.org>
2019-05-16 23:41:37 -0400

    imported tripRouter and implemented corresponding middleware

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const cors = require('cors')
 const app = express()
 const port = 5000
 
+const tripRouter = require('./routes/tripRouter');
+
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 app.use(cors())
 
+
+app.use('/trip', tripRouter);
 
 app.get('/', (req, res) => {
     res.send('hello')
