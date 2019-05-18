2534102392ea1b68d6431ad2ded978cfe7663dd2
Srirupa <srirupadatta@pursuit.org>
2019-05-18 14:04:43 -0400

    added function to create item

--- services/itemService.js
+++ services/itemService.js
@@ -1,4 +1,14 @@
 const db = require('./dbConnect');
 const ItemService = {};
 
+ItemService.create = (name, packed, quantity, bag_id, category_id) => {
+const sql = `
+INSERT INTO items (name, packed, quantity, bag_id, category_id)
+VALUES ($[name], $[packed], $[quantity], $[bag_id], $[category_id])
+RETURNING id;
+`
+return db.one(sql, { name, packed, quantity, bag_id, category_id })
+}
+
+
 module.exports = ItemService;
