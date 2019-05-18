b086660cb9e994ab5eb5d7802c29b3c7d2fe9f91
Srirupa <srirupadatta@pursuit.org>
2019-05-18 16:59:38 -0400

    added function to update an item

--- services/itemService.js
+++ services/itemService.js
@@ -10,5 +10,21 @@ RETURNING id;
 return db.one(sql, { name, packed, quantity, bag_id, category_id })
 }
 
+ItemService.update = (data, id) => {
+    const keys = Object.keys(data);
+    let sql = 'UPDATE items SET ';
+
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] === 'id') continue;
+        if (i === keys.length - 1) {
+            sql += `${keys[i]}=` + '$' + `[${keys[i]}] `
+        } else {
+            sql += `${keys[i]}=` + '$' + `[${keys[i]}], `
+        };
+    };
+    sql += 'WHERE id = $[id];';
+    return db.none(sql, { ...data, id });
+}
+
 
 module.exports = ItemService;
