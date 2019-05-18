b16c1c32db311c7924de0db664f38ccc4b4707de
Srirupa <srirupadatta@pursuit.org>
2019-05-18 14:32:10 -0400

    added route to create item

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -2,6 +2,17 @@ const express = require('express');
 const itemRouter = express.Router();
 const ItemService = require('../services/itemService');
 
+itemRouter.post('/', (req, res, next) => {
+    const { name, packed, quantity, bag_id, category_id } = req.body;
 
+    ItemService.create(name, packed, quantity, bag_id, category_id)
+        .then(({ id }) => {
+            res.status(200);
+            res.json({ id });
+        })
+        .catch(err => {
+            next(err);
+        })
+});
 
 module.exports = itemRouter;
