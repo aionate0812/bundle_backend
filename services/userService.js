be7a7b5298ceed3c5a10882d7389d06fcc34fc2e
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-23 01:22:49 -0400

    Created UserService function to read a user by uid

--- services/userService.js
+++ services/userService.js
@@ -7,7 +7,17 @@ UserService.createUser = (uid, username, email) => {
         VALUES ($[uid], $[username], $[email])
         RETURNING id;
     `;
-    return db.one(sql,{uid, username, email});
+    return db.one(sql, { uid, username, email });
+};
+
+UserService.readUserByUid = uid => {
+    const sql = `
+        SELECT *
+        FROM users
+        WHERE users.uid = $[uid]
+    `;
+
+    return db.one(sql, { uid });
 };
 
 module.exports = UserService;
