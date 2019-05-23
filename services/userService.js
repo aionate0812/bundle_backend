98a5e78052010c1c0123085ce56f399f420a2f43
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-23 01:17:45 -0400

    Created UserService function to create a user

--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,13 @@
 const db = require('./dbConnect');
 const UserService = {};
 
+UserService.createUser = (uid, username, email) => {
+    const sql = `
+        INSERT INTO users (uid, username, email)
+        VALUES ($[uid], $[username], $[email])
+        RETURNING id;
+    `;
+    return db.one(sql,{uid, username, email});
+};
+
 module.exports = UserService;
