249e7538df79c42db5424a6e4d1dd30c121292bb
Pamela Abreu <pamelaabreu@pursuit.com>
2019-05-23 16:27:04 -0400

    Made Post query for user route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,4 +2,15 @@ const express = require('express');
 const userRouter = express.Router();
 const UserService = require('../services/userService');
 
+userRouter.post('/', (req, res, next) => {
+    const { uid, username, email } = req.body;
+
+    UserService.createUser(uid, username, email)
+    .then(({ id }) => {
+        res.status(200)
+        res.json(id)
+    })
+    .catch(err => next(err))
+});
+
 module.exports = userRouter;
