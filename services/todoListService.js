0897fbfaedb66bcb503f3f9b742fc6e312d7d82e
Robert Abreu <robertabreu@pursuit.org>
2019-05-26 00:34:04 -0400

    created todolist cud services

---
+++ services/todoListService.js
@@ -0,0 +1,35 @@
+const db = require('./dbConnect');
+
+export const createTodoList = (name, trip_id) => {
+    const sql = `
+    INSERT INTO todolist (name, trip_id)
+    VALUES ($[name], $[trip_id])
+    RETURNING id;
+    `;
+    return db.one(sql, { name, trip_id });
+}
+
+export const updateTodoList = (data, id) => {
+    const keys = Object.keys(data);
+    let sql = 'UPDATE todolist SET ';
+
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] === 'id') continue;
+        if (i === keys.length - 1) {
+            sql += `${keys[i]}=` + '$' + `[${keys[i]}] `
+        } else {
+            sql += `${keys[i]}=` + '$' + `[${keys[i]}], `
+        };
+    };
+    sql += 'WHERE id = $[id];';
+    return db.none(sql, { ...data, id });
+};
+
+export const deleteTodoList = (id) => {
+    const sql = `
+    DELETE FROM todolist t
+    WHERE t.id = $[id]
+    `;
+    return db.none(sql, { id });
+
+};
