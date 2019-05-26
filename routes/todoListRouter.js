d6267ee0f7544e781eb7410e5f6dbe5cc0e49d1b
Robert Abreu <robertabreu@pursuit.org>
2019-05-26 00:40:54 -0400

    created todolist router, with post route for todolist creation

---
+++ routes/todoListRouter.js
@@ -0,0 +1,16 @@
+const todoListRouter = require('express').Router();
+const { createTodoList, updateTodoList, deleteTodoList } = require('../services/todoListService');
+
+todoListRouter.post('/', (req, res, next) => {
+    const { name, trip_id } = req.body;
+    createTodoList(name, trip_id)
+    .then(({ id }) => {
+        res.status(200);
+        res.json({
+             id,
+        });
+    })
+    .catch(err => {
+        next(err)
+    });
+});
