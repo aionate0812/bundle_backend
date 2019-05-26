dfff209836aca709c87526de0d64ecc7670ce1fa
Srirupa <srirupadatta@pursuit.org>
2019-05-26 00:55:32 -0400

    initial setup of categoryRouter

---
+++ routes/categoryRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const categoryRouter = express.Router();
+const CategoryService = require('../services/categoryService');
+
+
+
+module.exports = categoryRouter;
