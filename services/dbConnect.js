3f49c0b2633cd912e837a0571c9f69b07932909c
Alexander <aionate0812@gmail.com>
2019-05-26 23:26:06 -0400

    changed enrvironment variables

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')({})
-const config = require('../config.json' || process.env.CONFIG)
+const config = require(process.env.CONFIG || '../config.json')
 const db = pgp(process.env.DATABASE_URL || config.dba)
 
 module.exports = db
