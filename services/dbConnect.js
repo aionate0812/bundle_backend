02aea3c2b742bb20b6f1474d2aadc453d27fb4c9
Alexander <aionate0812@gmail.com>
2019-05-26 22:52:42 -0400

    added environment variables

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')({})
 const config = require('../config.json')
-const db = pgp(config.dba)
+const db = pgp(process.env.DATABASE_URL || config.dba)
 
 module.exports = db
 
