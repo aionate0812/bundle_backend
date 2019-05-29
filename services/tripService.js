2ec67a8dd6e63dc1a7c65a90a66989fa2672f901
Robert Abreu <robertabreu@pursuit.org>
2019-05-29 16:57:24 -0400

    Init Trip details (#25)
    * created function that retrieves all lists associated with a trip
    
    * created get bag services for trip
    
    * modified services to only include required columns
    
    * create trip init route, returns basic bag and list info about trip
    
    * formated tripServices
    
    * moved function comment outside of function; added async/await functionality to route
    
    * added some missing semi-colons
    
    * added some missing semi-colons

--- services/tripService.js
+++ services/tripService.js
@@ -7,8 +7,15 @@ TripService.create = (name, country, city, departure_date, return_date, user_id)
     VALUES ($[name], $[country], $[city], $[departure_date], $[return_date], $[user_id])
     RETURNING id;
     `;
-    return db.one(sql, { name, country, city, departure_date, return_date, user_id });
-}
+    return db.oneOrNone(sql, {
+        name,
+        country,
+        city,
+        departure_date,
+        return_date,
+        user_id
+    });
+};
 
 TripService.read = (id) => {
     const sql = `
@@ -17,7 +24,7 @@ TripService.read = (id) => {
     WHERE t.id = $[id]
     `;
     return db.oneOrNone(sql, { id });
-}
+};
 
 TripService.updateTrip = (data, id) => {
     const keys = Object.keys(data);
@@ -32,15 +39,46 @@ TripService.updateTrip = (data, id) => {
         };
     };
     sql += 'WHERE id = $[id];';
-    return db.none(sql, { ...data, id });
-}
+    return db.oneOrNone(sql, {
+        ...data,
+        id
+    });
+};
+
+TripService.getAllListsByTripID = (id) => {
+    const sql = `
+        SELECT t.id AS trip_id, tl.id AS todolist_id, tl.name
+        FROM trips t
+        JOIN todolist tl
+        ON t.id = tl.trip_id
+        WHERE t.id = $[id]
+    `;
+    return db.any(sql, {
+        id
+    });
+};
+
+TripService.getAllBagsByTripID = (id) => {
+    const sql = `
+    SELECT t.id AS trip_id, b.id AS bag_id, b.type_id
+    FROM trips t
+    JOIN bags b
+    ON t.id = b.trip_id
+    WHERE t.id = $[id]
+    `;
+    return db.any(sql, {
+        id
+    });
+};
 
 TripService.delete = (id) => {
     const sql = `
     DELETE FROM trips t
     WHERE t.id = $[id]
     `;
-    return db.none(sql, { id });
+    return db.oneOrNone(sql, {
+        id
+    });
 
 }
 
