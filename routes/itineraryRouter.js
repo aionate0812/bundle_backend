f25e11df27a1a4ffee4a35a1048c7bad4683bc2e
Alexander <aionate0812@gmail.com>
2019-06-08 23:32:00 -0400

    route to post itinerary

--- routes/itineraryRouter.js
+++ routes/itineraryRouter.js
@@ -6,9 +6,9 @@ const itineraryService = require('../services/itineraryService')
 
 itineraryRouter.get('/:trip_id', (req, res) => {
     const { trip_id } = req.params
-
     itineraryService.getItinerary(trip_id)
     .then( itineraryData => {
+        console.log(itineraryData)
         res.json(itineraryData)
     }, err => {
         console.log(err)
@@ -17,6 +17,33 @@ itineraryRouter.get('/:trip_id', (req, res) => {
     })
 })
 
+itineraryRouter.get('/types/all', (req, res) => {
+    itineraryService.getItineraryTypes()
+    .then( (itineraryTypes) => {
+        res.json(itineraryTypes)
+    }, err => {
+        console.log(err)
+        res.status(400)
+        res.json({msg:'Could not get itinerary types'})
+    })
+})
 
+itineraryRouter.post('/', (req, res) => {
+    const {name, address, phone, note, trip_id, itinerary_type} = req.body
+    itineraryService.addItinerary(name, address, phone, note, trip_id, itinerary_type)
+    .then( () => {
+        return itineraryService.getItinerary(trip_id) 
+    }, err => {
+        console.log(err)
+        res.json({msg:'Could not add itinerary'})
+    })
+    .then( itineraries => {
+        console.log(itineraries)
+        res.json({itineraries})
+    }, err => {
+        console.log(err)
+        res.json({msg:'Could not get itineraries'})
+    })
+})
 
 module.exports = itineraryRouter
--- services/itineraryService.js
+++ services/itineraryService.js
@@ -6,4 +6,12 @@ itineraryService.getItinerary = (trip_id) => {
     return db.manyOrNone('SELECT itinerary.*, itinerary_types.name as itinerary_name FROM itinerary INNER JOIN itinerary_types ON itinerary_types.id = itinerary.type_id WHERE trip_id = ${trip_id};', { trip_id })
 }
 
+itineraryService.getItineraryTypes = () => {
+    return db.manyOrNone('SELECT * FROM itinerary_types')
+}
+
+itineraryService.addItinerary = (name, address, phone, note, trip_id, itinerary_type) => {
+    return db.none('INSERT INTO itinerary (name, address, phone_number, note, trip_id, type_id) VALUES (${name}, ${address}, ${phone}, ${note}, ${trip_id}, ${itinerary_type})', {name, address, phone, note, trip_id, itinerary_type})
+}
+
 module.exports = itineraryService
